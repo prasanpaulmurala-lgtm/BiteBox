7e8ec66d80cf7b664a3d0145817deb4566e7892c
prasanpaulmurala-lgtm <prasanpaulmurala@gmail.com>
2026-07-27 15:21:30 +0530

    updated script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function login(){
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let error = document.getElementById("error");
-
     if(email === "admin@1234" && password === "1234"){
-
         localStorage.setItem("isloggedin","true")
         localStorage.setItem("user",email);
 
